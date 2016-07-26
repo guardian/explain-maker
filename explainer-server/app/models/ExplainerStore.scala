4edc1d9749118c9990fda41541d48e4dc68f33fc
Pascal Honoré <pascal.honore@theguardian.com>
2016-07-26 16:54:07 +0100

    setting all the fields of ContentChangeDetails

--- explainer-server/app/models/ExplainerStore.scala
+++ explainer-server/app/models/ExplainerStore.scala
@@ -49,8 +49,11 @@ class ExplainerStore @Inject() (config: Config) extends ExplainerAtomImplicits  
         }
       }
       val contentChangeDetails = ContentChangeDetails(
+        created      = explainer.contentChangeDetails.created,
         lastModified = Some(ChangeRecord(DateTime.now.getMillis, user=Some(User(user.email,Some(user.firstName),Some(user.lastName))))),
-        revision = explainer.contentChangeDetails.revision+1)
+        published    = explainer.contentChangeDetails.published,
+        revision = explainer.contentChangeDetails.revision+1
+      )
       val updatedExplainer = buildAtomWithDefaults(explainer.id, newExplainerAtom, contentChangeDetails, user)
       explainerDB.store(updatedExplainer)
       updatedExplainer
@@ -61,8 +64,11 @@ class ExplainerStore @Inject() (config: Config) extends ExplainerAtomImplicits  
     val uuid = java.util.UUID.randomUUID.toString
     val explainerAtom = ExplainerAtom("-", "-", DisplayType.Expandable)
     val contentChangeDetails = ContentChangeDetails(
+      created      = Some(ChangeRecord(DateTime.now.getMillis, user=Some(User(user.email,Some(user.firstName),Some(user.lastName))))),
       lastModified = Some(ChangeRecord(DateTime.now.getMillis, user=Some(User(user.email,Some(user.firstName),Some(user.lastName))))),
-      revision = 1)
+      published    = None,
+      revision = 1
+    )
     val explainer = buildAtomWithDefaults(uuid, explainerAtom, contentChangeDetails, user)
     explainerDB.store(explainer)
     Future(explainer)
@@ -72,8 +78,11 @@ class ExplainerStore @Inject() (config: Config) extends ExplainerAtomImplicits  
     explainerDB.load(id).map{ explainer =>
       val newExplainerAtom = ExplainerAtom(explainer.tdata.title, explainer.tdata.body, explainer.tdata.displayType)
       val contentChangeDetails = ContentChangeDetails(
-        lastModified = Some(ChangeRecord(DateTime.now.getMillis, user=Some(User(user.email,Some(user.firstName),Some(user.lastName))))),
-        revision = explainer.contentChangeDetails.revision+1)
+        created      = explainer.contentChangeDetails.created,
+        lastModified = explainer.contentChangeDetails.lastModified,
+        published    = Some(ChangeRecord(DateTime.now.getMillis, user=Some(User(user.email,Some(user.firstName),Some(user.lastName))))),
+        revision     = explainer.contentChangeDetails.revision+1
+      )
       val updatedExplainer = buildAtomWithDefaults(explainer.id, newExplainerAtom, contentChangeDetails, user)
       explainerDB.store(updatedExplainer)
       updatedExplainer
