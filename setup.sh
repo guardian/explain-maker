65dda55713ae7dbb8605af4d94d19de35c064911
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-08-18 11:03:08 +0100

    add .nvmrc file and update setup to use correct version of node

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 echo -e "${green}Install or update npm dependencies for explainer-server${plain}"
 
+nvm use
+
 # Install explainer-server dependencies and run a front end build
 pushd explainer-server
 npm install
