7f53aee85d016afb0471765f239bcc2fa69fec58
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-08-26 17:03:21 +0100

    reset url params when changing desk

--- explainer-client/src/main/scala/ExplainListJS.scala
+++ explainer-client/src/main/scala/ExplainListJS.scala
@@ -8,18 +8,10 @@ import scala.scalajs.js.URIUtils
 @JSExport
 object ExplainListJS {
 
-  def insertParam(key: String, value: String): Unit = {
+  def replaceParams(key: String, value: String): Unit = {
     val keyEnc = URIUtils.encodeURI(key)
     val valueEnc = URIUtils.encodeURI(value)
-
-    val parameters = dom.document.location.search.substring(1).split('&').map(p => {
-      if (p.startsWith(keyEnc)) {
-        s"$keyEnc=$valueEnc"
-      } else p
-    }).mkString("&")
-    val newQueryString = if (!parameters.contains(keyEnc)) s"$parameters&$keyEnc=$valueEnc" else parameters
-
-    dom.document.location.search = newQueryString
+    dom.document.location.search = s"$keyEnc=$valueEnc"
   }
 
   @JSExport
@@ -31,7 +23,7 @@ object ExplainListJS {
       val urlWithoutQueryParameters = s"${dom.document.location.protocol}//${dom.document.location.host}${dom.document.location.pathname}"
       dom.document.location.assign(urlWithoutQueryParameters)
     } else {
-      insertParam("desk", selectedDesk)
+      replaceParams("desk", selectedDesk)
     }
   }
 }
