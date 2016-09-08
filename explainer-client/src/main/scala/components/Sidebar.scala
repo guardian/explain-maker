25692a55fadce5eef8ba98b33eab31b6564f56ab
Philip McMahon <philip.mcmahon@guardian.co.uk>
2016-09-08 18:30:00 +0100

    wip

---
+++ explainer-client/src/main/scala/components/Sidebar.scala
@@ -0,0 +1,106 @@
+package components
+
+import api.Model
+import org.scalajs.dom._
+import org.scalajs.dom.html._
+import shared.models.{CsAtom, ExplainerUpdate}
+
+import scala.scalajs.js.Dynamic._
+import scala.util.{Failure, Success}
+import scalatags.JsDom.TypedTag
+import scalatags.JsDom.all._
+import org.scalajs.dom
+import shared.models.PublicationStatus._
+import views.ExplainEditor
+
+import scala.scalajs.js.Dynamic.{global => g}
+
+object Sidebar {
+
+  def title(explainer:CsAtom) = {
+
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val titleInput  = input(
+      id:="explainer-editor__title-wrapper__input",
+      cls:="form-field",
+      placeholder:="Explainer Title",
+      autofocus:=true
+    )(value := explainer.data.title).render
+
+    titleInput.onchange = (x: Event) => {
+      Model.updateFieldContent(explainer.id, ExplainerUpdate("title", titleInput.value)) onComplete {
+        case Success(_) => ExplainEditor.updateEmbedUrlAndStatusLabel(explainer.id, checkCapi=false)
+        case Failure(_) => g.console.error(s"Failed to update title with string ${titleInput.value}")
+      }
+    }
+  }
+
+  val embedUrlBox =
+    div(cls:="form-row")(
+      div(cls:="form-label")("Embed URL"),
+      textarea(
+        id:="interactive-url-text",
+        cls:="form-field form-field--text-area text-monospaced",
+        maxlength:=1800,
+        readonly:="true"
+      )("Unable to determine interactive url")
+    )
+
+  def republishembedURL(explainerId: String, status: PublicationStatus = Available) = {
+    val interactiveUrlText = status match {
+      case Available | UnlaunchedChanges => s"${g.CONFIG.INTERACTIVE_URL.toString}?id=$explainerId"
+      case Draft => "Publish explainer to get embed URL."
+      case TakenDown => "The explainer has been taken down. Republish to get URL."
+    }
+    dom.document.getElementById("interactive-url-text").textContent = interactiveUrlText
+  }
+
+  def displayTypeToggle(displayType: String) = {
+    val checkboxClassName = "explainer-editor__displayType-checkbox"
+    val checkbox: TypedTag[Input] = displayType match {
+      case "Expandable" =>
+        input(
+          cls:=checkboxClassName,
+          `type`:="checkbox",
+          `checked`:= "checked"
+        )
+      case "Flat" =>
+        input(
+          cls:=checkboxClassName,
+          `type`:="checkbox"
+        )
+    }
+    val checkboxTag = checkbox.render
+    checkboxTag.onchange = (x: Event) => {
+      g.updateCheckboxState()
+    }
+  }
+
+  def sidebar(explainer: CsAtom) = {
+    form()(
+      div(cls:="form-row")(
+        div(cls:="form-label")("Explainer Title"),
+        title(explainer)
+      ),
+      embedUrlBox,
+      div(cls:="form-row")(
+        div()(
+          displayTypeToggle(explainer.data.displayType), " Expandable explainer"
+        )
+      ),
+      div(cls:="explainer-editor__tag-management-wrapper")(
+        div(
+          id:="explainer-editor__commissioning-desk-tags-wrapper",
+          cls:="form-row")(
+          TagPickers.makeCommissioningDeskArea(explainer)
+        ),
+        div(
+          id:="explainer-editor__tags-wrapper",
+          cls:="form-row")(
+          TagPickers.makeTagArea(explainer)
+        )
+      )
+    ).render
+  }
+
+}
---
+++ explainer-client/src/main/scala/components/TagPickers.scala
@@ -0,0 +1,121 @@
+package components
+
+import api.Model
+import models.Tag
+import org.scalajs.dom
+import org.scalajs.dom._
+import org.scalajs.dom.html._
+import services.CAPIClient
+import shared.models.CsAtom
+import models.{Tag => CapiTag}
+import scala.scalajs.js.Dynamic._
+import scalatags.JsDom
+import scalatags.JsDom.TypedTag
+import scalatags.JsDom.all._
+import scala.scalajs.js.Dynamic.{global => g}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object TagPickers {
+
+  def redisplayExplainerTagManagementAreas(explainer: CsAtom): Unit = {
+    dom.document.getElementById("explainer-editor__commissioning-desk-tags-wrapper").innerHTML = ""
+    dom.document.getElementById("explainer-editor__commissioning-desk-tags-wrapper").appendChild(makeCommissioningDeskArea(explainer).render)
+
+    dom.document.getElementById("explainer-editor__tags-wrapper").innerHTML = ""
+    dom.document.getElementById("explainer-editor__tags-wrapper").appendChild(makeTagArea(explainer).render)
+  }
+
+  def tagDeleteButton(explainer:CsAtom, tagId:String) = {
+    val deleteButton = button(
+      cls:="tag__delete",
+      `type`:="button",
+      data("explainer-id"):=explainer.id,
+      data("tag-id"):=tagId
+    )("Delete").render
+    deleteButton.onclick = (x: Event) => {
+      Model.removeTagFromExplainer(explainer.id, tagId).map { explainer =>
+        redisplayExplainerTagManagementAreas(explainer)
+      }
+    }
+    deleteButton
+  }
+
+  def explainerToDivTags(explainer:CsAtom, filterLambda: String => Boolean) = {
+    explainer.data.tags.map(_.filter(filterLambda).map(tagId => {
+      div(cls:="tag")(
+        tagId,tagDeleteButton(explainer,tagId)
+      )
+    })).getOrElse(List())
+  }
+
+  def renderTaggingArea(explainer:CsAtom, suggestionsDomId: String, fieldDescription:String, inputTag: JsDom.Modifier, explainerToDivFilterLambda: String => Boolean) ={
+    div()(
+      div(
+        id:="explainer-editor__tags__input-field-wrapper",
+        cls:="relative-parent")(
+        div(cls:="form-group")(
+          div("")(
+            label(cls:="form-label")(fieldDescription)
+          ),
+          div("")(
+            inputTag
+          ),
+          div(id:=suggestionsDomId, cls:="tag__suggestions")("")
+        )
+      ),
+      div(cls:="tags")(
+        explainerToDivTags(explainer, explainerToDivFilterLambda)
+      )
+    )
+  }
+
+  def renderSuggestionSet(tags:List[CapiTag], explainerId:String, suggestionsDivIdentifier:String) = {
+    g.jQuery(s"#$suggestionsDivIdentifier").empty()
+    tags.foreach( tagObject => {
+      val node = div(cls:="tag__result")(tagObject.webTitle).render
+      node.onclick = (x: Event) => {
+        Model.addTagToExplainer(explainerId, tagObject.id).map { explainer =>
+          redisplayExplainerTagManagementAreas(explainer)
+        }
+      }
+      dom.document.getElementById(suggestionsDivIdentifier).appendChild(node)
+    })
+  }
+
+  def makeTagArea(explainer: CsAtom) = {
+    val suggestionsDivIdentifier = "explainer-editor__tags__suggestions"
+    val tagsSearchInput: TypedTag[Input] = input(
+      id:="explainer-editor__tags__tag-search-input-field",
+      cls:="form-field",
+      placeholder:="Tag search"
+    )
+    val tagsSearchInputTag = tagsSearchInput().render
+    tagsSearchInputTag.oninput = (x: Event) => {
+      val queryValue: String = g.readValueAtDiv("explainer-editor__tags__tag-search-input-field").asInstanceOf[String]
+
+      CAPIClient.capiTagRequest(Seq("type" -> "keyword", "q" -> queryValue))
+        .map(renderSuggestionSet(_, explainer.id, suggestionsDivIdentifier))
+
+    }
+    renderTaggingArea(explainer, suggestionsDivIdentifier, "Tags", tagsSearchInputTag, { tagId => !tagId.startsWith("tracking") })
+  }
+
+  def makeCommissioningDeskArea(explainer: CsAtom) = {
+    val suggestionsDivIdentifier = "explainer-editor__commissioning-desk-tags__suggestions"
+    val tagsSearchInput: TypedTag[Input] = input(
+      id:="explainer-editor__commissioning-desk-tags__tag-search-input-field",
+      cls:="form-field",
+      placeholder:="Commissioning desk"
+    )
+    val tagsSearchInputTag = tagsSearchInput().render
+    tagsSearchInputTag.oninput = (x: Event) => {
+      val queryValue: String = g.readValueAtDiv("explainer-editor__commissioning-desk-tags__tag-search-input-field").asInstanceOf[String]
+
+      CAPIClient.capiTagRequest(Seq("type" -> "tracking", "q" -> queryValue))
+        .map(renderSuggestionSet(_, explainer.id, suggestionsDivIdentifier))
+
+    }
+    renderTaggingArea(explainer, suggestionsDivIdentifier, "Commissioning Desk", tagsSearchInputTag, { tagId => tagId.startsWith("tracking") })
+  }
+
+}
