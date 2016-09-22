a8b4fd970c273ffcef6418eb8795039324158cbd
Shaun Dillon <shaun.dillon@guardian.co.uk>
2016-09-22 15:46:04 +0100

    show query in search box when searching

--- explainer-server/public/javascripts/explain-list-plain.js
+++ explainer-server/public/javascripts/explain-list-plain.js
@@ -1,3 +1,11 @@
-$('#search-form').submit(function(e){
+const searchForm = $('#search-form');
+const searchField = $('#explainer-search');
+var searchQuery = document.location.search.substring(1).split('&').find(p => p.startsWith('titleQuery')).split('=')[1];
+
+// Stop the page reloading
+searchForm.submit(function(e){
   e.preventDefault()
 });
+
+
+searchField.val(searchQuery);
